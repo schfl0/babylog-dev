958ef0b66aba4283dbf824800fa44171b35f375d
fl0 <77931563+schfl0@users.noreply.github.com>
2025-11-07 11:20:40 +0100

    Update local

--- app/components/TodayTempLogs.jsx
+++ app/components/TodayTempLogs.jsx
@@ -8,7 +8,7 @@ export default function TodayTempLogs({ tempLogs }) {
 
   return (
     <div className="rounded-md bg-red-50 px-2 py-4 text-xs shadow-md">
-      <h2 className="text-md font-bold">🌡️ Temps</h2>
+      <h2 className="text-sm font-bold">🌡️ Temps</h2>
       <div className="mt-4 flex flex-col justify-center">
         {filteredTemps.map((log, index) => {
           return <TempTodayItem log={log} key={index} />;
