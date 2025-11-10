36ee80017d18027cceec907b602a151378be2cc5
fl0 <77931563+schfl0@users.noreply.github.com>
2025-11-10 10:13:27 +0100

    Overview delete functionality

--- app/components/OverviewAll.jsx
+++ app/components/OverviewAll.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { getAllLogsDesc } from "../utils";
 
 import OverviewBottleAllItem from "./OverviewBottleAllItem";
@@ -29,6 +29,21 @@ export default function OverviewAll({
     med: OverviewMedAllItem,
   };
 
+  useEffect(() => {
+    setFilteredOverview(
+      getAllLogsDesc(
+        bottleLogs,
+        foodLogs,
+        napLogs,
+        poopLogs,
+        tempLogs,
+        medLogs,
+      ),
+    );
+  }, [bottleLogs, foodLogs, napLogs, poopLogs, tempLogs, medLogs]);
+
+  const [isEdit, setIsEdit] = useState(null);
+
   return (
     <div className="rounded-md px-2 py-4 text-xs shadow-md">
       <h2 className="text-sm font-bold">🔭 Overview</h2>
@@ -37,7 +52,14 @@ export default function OverviewAll({
           filteredOverview.map((log, index) => {
             const ItemComponent = overviewItems[log.log];
             if (!ItemComponent) return null;
-            return <ItemComponent log={log} key={index} />;
+            return (
+              <ItemComponent
+                log={log}
+                key={index}
+                isEdit={isEdit}
+                setIsEdit={setIsEdit}
+              />
+            );
           })
         ) : (
           <p>No logs yet.</p>
--- app/components/OverviewBottleAllItem.jsx
+++ app/components/OverviewBottleAllItem.jsx
@@ -1,5 +1,13 @@
+import { useFetcher } from "react-router";
 import { formatTime, formatDate } from "../utils";
 export default function OverviewBottleAllItem({ log: bottle }) {
+  const fetcher = useFetcher();
+
+  function handleClick() {
+    if (!isEdit) setIsEdit(bottle.id);
+    return;
+  }
+
   return (
     <div className="flex items-center justify-start bg-yellow-50 p-1">
       <div className="flex flex-1 items-center justify-between">
@@ -15,6 +23,19 @@ export default function OverviewBottleAllItem({ log: bottle }) {
       <div className="ml-6 flex items-center justify-end">
         <p>{formatDate(bottle.date)}</p>
       </div>
+      <button
+        className="ml-2 cursor-pointer hover:opacity-60"
+        onClick={handleClick}
+      >
+        📝
+      </button>
+      <fetcher.Form method="post" action="/delete-log" className="ml-1">
+        <input type="hidden" name="id" id="id" value={bottle.id} />
+        <input type="hidden" name="log" id="log" value={bottle.log} />
+        <button className="cursor-pointer transition-all hover:opacity-60">
+          ❌
+        </button>
+      </fetcher.Form>
     </div>
   );
 }
