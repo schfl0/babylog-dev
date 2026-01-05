4aa93d4cc970317352af3860734fdf110d9a4d6d
fl0 <77931563+schfl0@users.noreply.github.com>
2026-01-05 20:30:21 +0100

    Upate all view routes

--- app/components/DateFilter.jsx
+++ app/components/DateFilter.jsx
@@ -1,4 +1,4 @@
-export default function DateFilter({ dateFilter, setDateFilter }) {
+export default function DateFilter({ onChange }) {
   return (
     <div className="flex flex-1 items-center justify-end gap-2">
       <label htmlFor="dateFilter">
@@ -8,14 +8,13 @@ export default function DateFilter({ dateFilter, setDateFilter }) {
           type="date"
           id="dateFilter"
           name="dateFilter"
-          value={dateFilter}
-          onChange={(e) => setDateFilter(e.target.value)}
+          onChange={(e) => onChange(e.target.value)}
         />
       </label>
       <button
         className="tranisition-all text-2xs cursor-pointer rounded-sm bg-pink-600 px-2 py-1 text-white hover:opacity-60"
         type="button"
-        onClick={() => setDateFilter("")}
+        onClick={() => onChange("")}
       >
         All dates
       </button>
