40c1e5f974ce089cb89e4197799e3fc0df269123
fl0 <77931563+schfl0@users.noreply.github.com>
2026-05-02 11:33:39 +0200

    Prevent zoom in

--- app/root.tsx
+++ app/root.tsx
@@ -32,7 +32,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1"
+        />
         <Meta />
         <Links />
         <link
