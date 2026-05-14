26953524595e3f896b89dcfd038bcc7e7817efd5
fl0 <77931563+schfl0@users.noreply.github.com>
2026-05-14 09:45:45 +0200

    Revise UI

--- app/components/BottleItemEdit.jsx
+++ app/components/BottleItemEdit.jsx
@@ -26,28 +26,47 @@ export default function BottleItemEdit({ bottle, setIsEdit }) {
       className="text-3xs mt-1 flex w-full flex-col items-start gap-1 border-t border-gray-300 pt-1"
     >
       <input type="hidden" name="id" id="id" value={bottle.id} />
-      <div className="flex flex-col gap-0.5">
-        <label htmlFor="type" className="font-medium">
-          Type
-        </label>
-        <select
-          className="rounded-sm border border-gray-400 bg-yellow-50 px-1 py-0.5"
-          name="type"
-          id="type"
-          value={inputType}
-          onChange={(e) => setInputType(e.target.value)}
-        >
-          {options.map((option) => (
-            <option key={option} value={option}>
-              {capitalizeStr(option)}
-            </option>
-          ))}
-        </select>
-        {fetcher?.data?.bottle?.[0] && (
-          <p className="mt-0.5 text-[8px] leading-none text-red-500">
-            {fetcher.data.bottle[0]}
-          </p>
-        )}
+      <div className="flex w-full items-center justify-between">
+        <div className="flex flex-col gap-0.5">
+          <label htmlFor="type" className="font-medium">
+            Type
+          </label>
+          <select
+            className="rounded-sm border border-gray-400 bg-yellow-50 px-1 py-0.5"
+            name="type"
+            id="type"
+            value={inputType}
+            onChange={(e) => setInputType(e.target.value)}
+          >
+            {options.map((option) => (
+              <option key={option} value={option}>
+                {capitalizeStr(option)}
+              </option>
+            ))}
+          </select>
+          {fetcher?.data?.bottle?.[0] && (
+            <p className="mt-0.5 text-[8px] leading-none text-red-500">
+              {fetcher.data.bottle[0]}
+            </p>
+          )}
+        </div>
+        <div className="mt-1 flex gap-2 text-sm">
+          <button
+            type="submit"
+            className="cursor-pointer transition-all hover:opacity-60"
+          >
+            ✅
+          </button>
+          <button
+            type="button"
+            className="cursor-pointer transition-all hover:opacity-60"
+            onClick={() => {
+              setIsEdit(null);
+            }}
+          >
+            🚫
+          </button>
+        </div>
       </div>
 
       <div className="flex flex-col gap-0.5">
@@ -106,24 +125,6 @@ export default function BottleItemEdit({ bottle, setIsEdit }) {
         id="timezoneOffset"
         value={new Date().getTimezoneOffset()}
       />
-
-      <div className="mt-1 flex gap-2 text-sm">
-        <button
-          type="submit"
-          className="cursor-pointer transition-all hover:opacity-60"
-        >
-          ✅
-        </button>
-        <button
-          type="button"
-          className="cursor-pointer transition-all hover:opacity-60"
-          onClick={() => {
-            setIsEdit(null);
-          }}
-        >
-          🚫
-        </button>
-      </div>
     </fetcher.Form>
   );
 }
